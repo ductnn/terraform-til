c19b2b51d39e088185def7b1b9592998b4712ec1
ductnn <trannhuduc531998@gmail.com>
2021-04-16 19:38:50 +0700

    Update

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,15 +5,15 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-013f17f36f8b1fefb"
+    # us-west-2 = "ami-06b94666"
+    # eu-west-1 = "ami-0d729a60"
   }
 }
 
