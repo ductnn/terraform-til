3500a0178c50fa11479570a2879c61f87fc3d2db
ductnn <trannhuduc531998@gmail.com>
2021-04-19 02:54:54 +0700

    FIX script install nginx

--- ductn/demo/scripts/script.sh
+++ ductn/demo/scripts/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # sleep until instance is ready
-until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
-  sleep 1
-done
+# until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+#   sleep 1
+# done
 
 # install nginx
 apt-get update
