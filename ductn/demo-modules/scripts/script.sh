47b63c4bb9b52c415b3f6e929001c7da871fe801
ductnn <trannhuduc531998@gmail.com>
2021-04-25 20:27:42 +0700

    Remove install docker

--- ductn/demo-modules/scripts/script.sh
+++ ductn/demo-modules/scripts/script.sh
@@ -13,27 +13,23 @@ apt-get -y install nginx
 service nginx start
 
 # install docker
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt-get update
-sudo apt install docker-ce -y
+# sudo apt-get update && sudo apt-get upgrade -y
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+# sudo apt-get update
+# sudo apt install docker-ce -y
 
-# Install docker-compose
-sudo apt-get update && sudo apt-get upgrade -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# # Install docker-compose
+# sudo apt-get update && sudo apt-get upgrade -y
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
-# Install git
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install git -y
+# # Install git
+# sudo apt-get update && sudo apt-get upgrade -y
+# sudo apt-get install git -y
 
 # Install pip3
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install python3-pip -y
 sudo apt-get install python3-venv -y
-
-# Clone source code netbox
-git clone https://github.com/netbox-community/netbox.git
-git clone https://github.com/ductnn/netbox-docker.git
