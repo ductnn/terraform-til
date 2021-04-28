c7e216609f23db1cd02f6261f32080be9995dbe6
ductnn <trannhuduc531998@gmail.com>
2021-04-29 04:00:03 +0700

    Update

--- ductn/demo-modules/modules/securitygroup/main.tf
+++ ductn/demo-modules/modules/securitygroup/main.tf
@@ -112,7 +112,6 @@ resource "aws_security_group" "db-securitygroup" {
         to_port     = 0
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-        self        = true
     }
 
     tags = {
