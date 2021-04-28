dba81a6bccd97439cf73654e9f71f3789d9af529
Duc Tran <trannhuduc531998@gmail.com>
2021-04-29 04:05:54 +0700

    Remove key

--- Demo/variables.tf
+++ Demo/variables.tf
@@ -5,11 +5,11 @@ variable "aws_resource" {
 variable "AWS_Access_Key" {
     description = "access key"
     type = string
-    default = "AKIAXE4N45FWKKVATCOH"
+#     default = "AKIAXE4N45FWKKVATCOH"
 }
 
 variable "AWS_Secret_Key" {
     description = "secret_key"
     type = string
-    default = "REibRZOWOdOPk8qxUEWD6fqNJQo80W0T7MLv2WeB"
+#     default = "REibRZOWOdOPk8qxUEWD6fqNJQo80W0T7MLv2WeB"
 }
