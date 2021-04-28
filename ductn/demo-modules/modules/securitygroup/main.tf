e8b831762d80e9af517c4a784f9595e5be828da7
ductnn <trannhuduc531998@gmail.com>
2021-04-28 23:18:09 +0700

    Add egress DataBase security group

--- ductn/demo-modules/modules/securitygroup/main.tf
+++ ductn/demo-modules/modules/securitygroup/main.tf
@@ -104,7 +104,15 @@ resource "aws_security_group" "db-securitygroup" {
         from_port   = 5432
         to_port     = 5432
         protocol    = "tcp"
+        security_groups = [aws_security_group.myinstance.id]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
+        self        = true
     }
 
     tags = {
